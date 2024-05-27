a215b3b5d30fa5ee4579f511095d38f199f85ca6
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-05-28 02:44:39 +0900

    과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { Fragment } from "react";
-import { useState } from "react"
+import { useState } from "react";
+
 function App() {
   const [count, setCount] = useState(0);
-  const [isDark, setIsDark] = useState();
+  const addCount = () => {setCount(count + 1)} // 1 증가
+  const minusCount = () => {setCount(count - 1)} // 1 감소
+  const reset = () => {setCount(0);} // reset
 
   return (
     <Fragment>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button>+</button>
-        <button>-</button>
-        <button>reset</button>
-      </div>
+      <main>
+        <div className="container">
+          <h2>count : {count}</h2>
+          <button onClick={addCount}>+</button>
+          <button onClick={minusCount}>-</button>
+          <button onClick={reset}>reset</button>
+        </div>
+      </main>
     </Fragment>
   )  
 }
+
 export default App;
