64a753ead6bba8ca0cefbccf264b26c8c18da093
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-05-30 11:46:55 +0900

    2번째 과제완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value)
+  }
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    if(!input.trim()) return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: input,
+      completed: false,
+    };
+    
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleToggleTodo = (id) => {
+    setTodos(todos.map((todo) => {todo.id === id ? {...todo, completed: !todo.completed} : todo}))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <h1>할 일 목록</h1>
+        <form onSubmit={handleAddTodo}>
+          <input type="text" placeholder="할 일을 추가 해 보세요!" value={input} onChange={handleInputChange} />
+          <button type="submit">추가</button>
+        </form>
+        <ul>
+          {todos.map((todo) => {
+            <li key={todo.id} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+              {todo.text}
+              <button onClick={() => {handleToggleTodo(todo.id)}}>
+                {todo.completed ? "취소" : "완료"}
+              </button>
+              <button onClick={() => {handleDeleteTodo(todo.id)}}>삭제</button>
+            </li>
+          })}
+        </ul>
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
